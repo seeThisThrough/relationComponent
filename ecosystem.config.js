669177e9c360575f0a2464fed3541a803190c7dc
seeThisThrough <geedyne.li@gmail.com>
2019-04-15 15:47:00 -0500

    updated EC2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps: [{
-    name: 'relationComponent',
-    script: 'server/index.js'
-  }],
+  apps: [
+    {
+      name: "relationComponent",
+      script: "server/index.js"
+    }
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-3-86-240-133.compute-1.amazonaws.com',
-      key: '~/.ssh/tutorial.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:microdynamicservices/relationComponent.git',
-      path: '/home/ubuntu/relationComponent',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      user: "ubuntu",
+      host: "ec2-54-163-133-224.compute-1.amazonaws.com",
+      key: "~/.ssh/tutorial.pem",
+      ref: "origin/master",
+      repo: "git@github.com:microdynamicservices/relationComponent.git",
+      path: "/home/ubuntu/relationComponent",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
-}
+};
